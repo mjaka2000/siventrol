abef57c2d8f0d4c64dd363b51d9383b2ec85519f
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2024-01-04 18:48:40 +0800

    update view admin "d_users"

---
+++ application/views/admin/d_users/profile.php
@@ -0,0 +1,191 @@
+<?php $this->load->view('template/head'); ?>
+<?php $this->load->view('admin/template/nav'); ?>
+<?php $this->load->view('admin/template/sidebar'); ?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Profile</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="<?= site_url('admin'); ?>"><i class="fas fa-home"></i></a></li>
+                        <li class="breadcrumb-item active">Profile</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div id="loading" class="tengah">
+            <img src="<?= base_url(); ?>assets/style/loading.gif" alt="loading" width="50%">
+        </div>
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card">
+
+                        <div class="card-body card-primary card-outline" align="center">
+                            <div class="info">
+                                <!-- <?php foreach ($avatar as $a) { ?>
+                                    <div class="image">
+                                        <img src="<?= base_url('assets/upload/user/' . $a->nama_file); ?>" class="profile-user-img img-fluid img-circle" alt="User Image">
+                                    <?php } ?> 
+                                </div>-->
+                                <strong><?= $this->session->userdata('nama_lengkap') ?></strong> sebagai Administrator
+                                <br><small>Last Login : <?= date('d-m-Y H:i:s', strtotime($this->session->userdata('last_login'))) ?></small>
+                            </div>
+                            <?php if ($this->session->flashdata('msg_gambar_sukses')) { ?>
+                                <div class="alert alert-success alert-dismissable">
+                                    <button class="close" data-dismiss="alert" aria-label="close">&times;</button>
+                                    <strong>Berhasil!</strong><br> <?= $this->session->flashdata('msg_gambar_sukses'); ?>
+                                </div>
+                            <?php } ?>
+                            <?php if ($this->session->flashdata('msg_gambar_error')) { ?>
+                                <div class="alert alert-warning alert-dismissable">
+                                    <button class="close" data-dismiss="alert" aria-label="close">&times;</button>
+                                    <strong>Peringatan!</strong><br> <?= $this->session->flashdata('msg_gambar_error'); ?>
+                                </div>
+                            <?php } ?>
+                            <?php if (isset($pesan_error)) { ?>
+                                <div class="alert alert-danger alert-dismissable">
+                                    <button class="close" data-dismiss="alert" aria-label="close">&times;</button>
+                                    <strong>Peringatan!</strong><br> <?= $pesan; ?>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-primary card-outline card-tabs">
+                            <div class="card-header p-2">
+                                <ul class="nav nav-tabs" id="custom-tabs-three-tab" role="tablist">
+                                    <li class="nav-item">
+                                        <a href="#settings" class="nav-link active" data-toggle="pill" href="#custom-tabs-three-home" role="tab" aria-controls="custom-tabs-three-home" aria-selected="true">Ganti Password</a>
+                                    </li>
+                                    <!-- <li class="nav-item">
+                                        <a href="#" class="nav-link" data-toggle="pill" href="#custom-tabs-three-profile" role="tab" aria-controls="custom-tabs-three-profile" aria-selected="false">Ganti Gambar</a>
+                                    </li> -->
+                                    <!-- <li class="nav-item">
+                                        <a href="#backupdb" class="nav-link" data-toggle="pill" href="#custom-tabs-three-profile" role="tab" aria-controls="custom-tabs-three-profile" aria-selected="false">Backup DB</a>
+                                    </li> -->
+                                </ul>
+                            </div>
+                            <div class="card-body">
+                                <div class="tab-content" id="custom-tabs-three-tabContent">
+                                    <div class="tab-pane fade" id="picture" role="tabpanel" aria-labelledby="custom-tabs-three-home-tab">
+                                        <form class="form-horizontal" action="<?= site_url('admin/proses_gambarupload'); ?>" method="post" enctype="multipart/form-data">
+                                            <div class="form-group row">
+                                                <label for="userpicture" class="col-sm-3 control-label">Pilih File</label>
+
+                                                <div class="col-sm-9">
+                                                    <input type="file" name="userpicture" class="form-control" id="userpicture">
+                                                    <small style="color: red;">
+                                                        <p>*File yang diijinkan "jpg|png|jpeg", max size 1MB.</p>
+                                                    </small>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="form-group " align="center">
+                                                <div class="col-sm-offset-2 col-sm-9">
+                                                    <button onclick="history.back(-1)" type="button" class="btn btn-sm btn-default" name="btn_kembali"><i class="fa fa-arrow-left mr-2"></i>Kembali</button>
+                                                    <button type="submit" class="btn btn-sm btn-success"><i class="fa fa-check mr-2"></i>Submit</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="tab-pane fade" id="backupdb" role="tabpanel" aria-labelledby="custom-tabs-three-home-tab">
+                                        <button onclick="window.location.href='<?= site_url('admin/backup_db'); ?>'" class="btn btn-sm btn-primary"><i class="fa fa-database"></i>&nbsp;Backup DB</button>
+                                    </div>
+
+                                    <div class="tab-pane fade show active" id="settings" role="tabpanel" aria-labelledby="custom-tabs-three-home-tab">
+                                        <?php if ($this->session->flashdata('msg_sukses')) { ?>
+                                            <div class="alert alert-success alert-dismissable">
+                                                <button class="close" data-dismiss="alert" aria-label="close">&times;</button>
+                                                <strong>Berhasil!</strong><br> <?= $this->session->flashdata('msg_sukses'); ?>
+                                            </div>
+                                        <?php } ?>
+                                        <?php if (validation_errors()) { ?>
+                                            <div class="alert alert-warning alert-dismissable">
+                                                <button class="close" data-dismiss="alert" aria-label="close">&times;</button>
+                                                <strong>Peringatan!</strong><br> <?php echo validation_errors(); ?>
+                                            </div>
+                                        <?php } ?>
+                                        <form class="form-horizontal" action="<?= site_url('admin/proses_newpassword'); ?>" method="post">
+                                            <div class="form-group row">
+                                                <label for="username" class="col-sm-3 control-label">Username</label>
+                                                <div class="col-sm-9">
+                                                    <input type="text" name="username" class="form-control" id="username" disabled value="<?= $this->session->userdata('username'); ?>">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="nama" class="col-sm-3 control-label">Nama</label>
+                                                <div class="col-sm-9">
+                                                    <input type="text" name="nama_lengkap" class="form-control" id="nama_lengkap" value="<?= $this->session->userdata('nama_lengkap'); ?>">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="new_password" class="col-sm-3 control-label">Password Baru</label>
+                                                <div class="col-sm-9">
+                                                    <input type="password" name="new_password" class="form-control" id="new_password" placeholder="Password Baru">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="confirm_new_password" class="col-sm-3 control-label">Konfirmasi Password Baru</label>
+                                                <div class="col-sm-9">
+                                                    <input type="password" name="confirm_new_password" class="form-control" id="confirm_new_password" placeholder="Konfirmasi Password Baru">
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="form-group" align="center">
+                                                <div class=" col-sm-9">
+                                                    <button onclick="history.back(-1)" type="button" class="btn btn-sm btn-default" name="btn_kembali">Kembali</button>
+                                                    <button type="submit" class="btn btn-sm btn-success">Simpan</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+                <!-- </div> -->
+            </div>
+
+        </div>
+</div><!-- /.container-fluid -->
+</section>
+</div>
+<?php $this->load->view('template/footer'); ?>
+
+<?php $this->load->view('template/script') ?>
+<script>
+    //* Script untuk menampilkan loading
+    document.onreadystatechange = function() {
+        if (document.readyState !== "complete") {
+            document.querySelector(
+                "body").style.visibility = "hidden";
+            document.querySelector(
+                "#loading").style.visibility = "visible";
+        } else {
+            document.querySelector(
+                "#loading").style.display = "none";
+            document.querySelector(
+                "body").style.visibility = "visible";
+        }
+    };
+</script>
+</body>
+
+</html>
---
+++ application/views/admin/d_users/tbl_users.php
@@ -0,0 +1,143 @@
+<?php $this->load->view('template/head'); ?>
+<?php $this->load->view('admin/template/nav'); ?>
+<?php $this->load->view('admin/template/sidebar'); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Data User</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="<?= site_url('admin'); ?>"><i class="fas fa-home"></i></a></li>
+                        <li class="breadcrumb-item active">User</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div id="loading" class="tengah">
+            <img src="<?= base_url(); ?>assets/style/loading.gif" alt="loading" width="50%">
+        </div>
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Data User
+                        </div>
+                        <div class="card-body">
+                            <?php if ($this->session->flashdata('msg_sukses')) { ?>
+                                <div class="alert alert-success alert-dismissible">
+                                    <button class="close" data-dismiss="alert" aria-label="close">&times;</button>
+                                    <strong>Berhasil!</strong><br> <?= $this->session->flashdata('msg_sukses'); ?>
+                                </div>
+                            <?php } ?>
+                            <button onclick="window.location.href='<?= site_url('admin/tambah_users'); ?>'" style="margin-bottom:10px;" type="button" class="btn btn-sm btn-primary" name="tambah_data"><i class="fa fa-plus mr-2" aria-hidden="true"></i>Tambah Data</button>
+
+                            <table id="example1" class="table table-bordered table-hover" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th style="width :10px">No.</th>
+                                        <th>Nama</th>
+                                        <th>Username</th>
+                                        <th>level</th>
+                                        <th>Last Login</th>
+                                        <th style="width:10%">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $no = 1;
+                                    if (is_array($list_data)) { ?>
+                                        <?php foreach ($list_data as $u) : ?>
+                                            <tr>
+                                                <td><?= $no++; ?></td>
+                                                <td><?= $u->nama_lengkap ?></td>
+                                                <td><?= $u->username ?></td>
+                                                <?php if ($u->level == 0) { ?>
+                                                    <td>User Admin</td>
+                                                <?php } else { ?>
+                                                    <td>User Biasa</td>
+                                                <?php } ?>
+                                                <td><?= date('d-m-Y H:i:s', strtotime($u->last_login))  ?></td>
+                                                <td>
+                                                    <a href="<?= site_url('admin/update_user/' . $u->id_user); ?>" type="button" class="btn btn-sm btn-info" name="btn_edit"><i class="fa fa-edit "></i></a>
+                                                    <a href="<?= site_url('admin/hapus_user/' . $u->id_user); ?>" type="button" title="Hapus" class="btn btn-sm btn-danger btn-delete" name="btn_delete"><i class="fa fa-trash "></i></a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+</div>
+<!-- /.content-wrapper -->
+<?php $this->load->view('template/footer'); ?>
+<?php $this->load->view('template/script') ?>
+
+<script type="text/javascript">
+    $(function() {
+        $('#example1').DataTable({
+            // 'paging': true,
+            // 'lengthChange': false,
+            // 'searching': false,
+            // 'ordering': false,
+            // 'info': true,
+            'responsive': true,
+            'autoWidth': false
+        })
+    }); //* Script untuk memuat datatable
+</script>
+<script>
+    //* Script untuk menampilkan loading
+    document.onreadystatechange = function() {
+        if (document.readyState !== "complete") {
+            document.querySelector(
+                "body").style.visibility = "hidden";
+            document.querySelector(
+                "#loading").style.visibility = "visible";
+        } else {
+            document.querySelector(
+                "#loading").style.display = "none";
+            document.querySelector(
+                "body").style.visibility = "visible";
+        }
+    };
+</script>
+
+<script type="text/javascript">
+    $('.btn-delete').on('click', function() {
+        var getLink = $(this).attr('href');
+        Swal.fire({
+            title: 'Hapus Data',
+            text: 'Yakin ingin menghapus data?',
+            type: 'warning',
+            confirmButtonColor: '#d9534f',
+            showCancelButton: true,
+        }).then(result => {
+            //jika klik ya maka arahkan ke proses.php
+            if (result.isConfirmed) {
+                window.location.href = getLink
+            }
+        })
+        return false;
+    }); //* Script untuk memuat sweetalert hapus data
+</script>
+</body>
+
+</html>
